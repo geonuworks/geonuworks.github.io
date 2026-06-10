85e831f0640fbb49dbe1a43d8e31e953eb8e797a
geonuworks <audiomachine@naver.com>
2026-06-10 21:52:42 +0900

    update

---
+++ sketch-data.js
@@ -0,0 +1,12 @@
+const sketchMeta = {
+  "01": { project: "BLOOM*", date: "2026" },
+  "02": { project: "N/A", date: "2025" },
+  "03": { project: "STORMGAZER", date: "2025" },
+  "04": { project: "N/A", date: "2023" },
+  "05": { project: "N/A", date: "2023" },
+  "06": { project: "STORMGAZER", date: "2025" },
+  "07": { project: "PLAY!", date: "2024" },
+  "08": { project: "N/A", date: "2023" },
+  "09": { project: "N/A", date: "2023" },
+  "10": { project: "SIDE PROJECT #1", date: "2024" }
+};
